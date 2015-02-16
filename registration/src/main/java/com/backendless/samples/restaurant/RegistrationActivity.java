696e3099f563c86cdd2d4bb8b3119828d84845cf
scadge <scadgek@live.com>
2015-02-16 12:53:07 +0200

    redesigned registration sample

--- registration/src/main/java/com/backendless/samples/restaurant/RegistrationActivity.java
+++ registration/src/main/java/com/backendless/samples/restaurant/RegistrationActivity.java
@@ -10,7 +10,6 @@ import android.widget.Toast;
 import com.backendless.Backendless;
 import com.backendless.BackendlessUser;
 import com.backendless.async.callback.AsyncCallback;
-import com.backendless.exceptions.BackendlessFault;
 
 /**
  * Handles registration flow.
